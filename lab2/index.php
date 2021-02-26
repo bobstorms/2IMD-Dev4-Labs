37f98211b664aa9705e03dce7854408578622bd9
Bob Storms <bob.storms@hotmail.be>
2021-02-26 01:24:23 +0100

    Finished Lab2

--- lab2/index.php
+++ lab2/index.php
@@ -1,4 +1,39 @@
-<!DOCTYPE html>
+<?php
+
+    session_start();
+
+    function canLogin($username, $password) {
+        if ($username === "bob.storms" && $password === "Test123") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if(!empty($_POST)) {
+        // Er is data verzonden
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if(canLogin($username, $password)) {
+            // LOGIN
+            session_start();
+            $_SESSION["loggedin"] = true;
+            $_SESSION["username"] = $username;
+        } else {
+            $error = true; 
+        }
+    }
+
+    if($_SESSION["loggedin"] == true) {
+        $loggedin = true;
+    }
+
+    if(!empty($_GET)) {
+        $page = $_GET["page"];
+    }
+
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -10,51 +45,84 @@
 </head>
 
 <body>
+    <?php if($loggedin == true): ?>
     <header>
         <nav class="nav">
-            <a href="?page=browser">Browse</a>
+            <a href="?page=browse">Browse</a>
             <a href="?page=getdesktop">Get desktop</a>
             <a href="?page=tryprime">Try prime</a>
             <a href="#" class="loggedIn">
                 <div class="user--avatar"><img
                         src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=ddcb7ec744fc63472f2d9e19362aa387"
                         alt=""></div>
-                <h3 class="user--name">Username here</h3>
+                <h3 class="user--name"><?php echo $_SESSION["username"]; ?></h3>
                 <span class="user--status">Watching dakotaz</span>
             </a>
-            <a href="logout">Log out?</a>
+            <a href="logout.php">Log out?</a>
         </nav>
     </header>
+    <?php endif; ?>
 
     <div id="app">
+
+        <?php if(!$loggedin): ?>
+
         <h1>Log in to Twitch</h1>
+
         <nav class="nav--login">
             <a href="#" id="tabLogin">Log in</a>
             <a href="#" id="tabSignIn">Sign up</a>
         </nav>
 
-        <div class="alert hidden">That password was incorrect. Please try again</div>
+        <?php if($error): ?>
+            <div class="alert">That password was incorrect. Please try again</div>
+        <?php endif; ?>
 
-        <div class="form form--login">
-            <label for="username">Username</label>
-            <input type="text" id="username">
+        <form action="" method="POST">
+            <div class="form form--login">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
 
-            <label for="password">Password</label>
-            <input type="password" id="password">
-        </div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
 
-        <div class="form form--signup hidden">
-            <label for="username2">Username</label>
-            <input type="text" id="username2">
+            <div class="form form--signup hidden">
+                <label for="username2">Username</label>
+                <input type="text" id="username2">
 
-            <label for="password2">Password</label>
-            <input type="password" id="password2">
+                <label for="password2">Password</label>
+                <input type="password" id="password2">
 
-            <label for="email">Email</label>
-            <input type="text" id="email">
-        </div>
+                <label for="email">Email</label>
+                <input type="text" id="email">
+            </div>
 
-        <a href="#" class="btn" id="btnSubmit">Log In</a>
+            <input type="submit" class="btn" id="btnSubmit" value="Log In"/>
+        </form>
+
+        <?php else: ?>
+
+        <h1>
+            <?php
+                switch($page) {
+                    case "browse":
+                        echo "Browse some streamers! 🕹";
+                        break;
+                    case "getdesktop":
+                        echo "Getting the desktop app! 💻";
+                        break;
+                    case "tryprime":
+                        echo "Let's try prime! ✌🏻";
+                        break;
+                    default:
+                        echo "Welcome to Twitch!";
+                }
+            ?>
+        </h1>
+
+        <?php endif; ?>
+
     </div>
 </body>
 
