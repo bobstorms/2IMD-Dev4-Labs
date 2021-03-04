a27a097e76d18c6fc92f7d3d793eb791bfdbbac6
Bob Storms <bob.storms@hotmail.be>
2021-03-04 21:21:10 +0100

    Register form checks for password lenth and match

--- lab3/register.php
+++ lab3/register.php
@@ -1,4 +1,26 @@
-<!DOCTYPE html>
+<?php 
+
+	session_start();
+
+	if(!empty($_POST)) {
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		$password_conf = $_POST["password_conf"];
+
+		if(strlen($password) >= 8) {
+			if($password === $password_conf) {
+				// rest code
+			} else {
+				$error = true;
+				$error_match = true;
+			}
+		} else {
+			$error = true;
+			$error_char = true;
+		}
+	}
+
+?><!DOCTYPE html>
 <html>
 <head>
 	<title></title>
@@ -16,25 +38,34 @@
 		<div class="liner"></div>
 		<div id="form">
 			<form method="post" action>
-				<input name="email" placeholder="Email" type="email" required autofocus /><input name="password" placeholder="Password" type="password" required /><input name="password_conf" placeholder="Confirm Password" type="password" required />
+				<input name="email" placeholder="Email" type="email" required autofocus />
+				<input name="password" placeholder="Password" type="password" required />
+				<input name="password_conf" placeholder="Confirm Password" type="password" required />
 				<h5>
 					Remember
 				</h5>
-				<input class="btn-toggle btn-toggle-round" id="btn-toggle-1" name="remember" type="checkbox" /><label for="btn-toggle-1"></label><input name="register" type="submit" value="Register" />
+				<input class="btn-toggle btn-toggle-round" id="btn-toggle-1" name="remember" type="checkbox" />
+				<label for="btn-toggle-1"></label>
+				<input name="register" type="submit" value="Register"/>
 			</form>
 		</div>
 		
 	</div>
 
-	
+	<?php if(isset($error)): ?>
 	<div class="user-messages-area">
 		<div class="alert alert-danger">
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 			<ul>
-				<li>Errors can go here</li>
+				<?php if(isset($error_match)): ?>
+				<li>The passwords you've entered, don't match.</li>
+				<?php elseif(isset($error_char)): ?>
+				<li>Your password should at least be 8 characters long.</li>
+				<?php endif; ?>
 			</ul>
 		</div>
 	</div>
+	<?php endif; ?>
 	
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
